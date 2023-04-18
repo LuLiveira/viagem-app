958820e13a0ee11cc9b25163c88b87bb371d8666
Lucas Silva <lcsd.lucas@gmail.com>
2023-04-18 08:41:01 -0300

    Finalizando CRUD de Cliente

--- viagem-app/src/main/java/com/lucassilva/cliente/ClienteResource.java
+++ viagem-app/src/main/java/com/lucassilva/cliente/ClienteResource.java
@@ -1,11 +1,14 @@
 package com.lucassilva.cliente;
 
+import java.net.URI;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.print.attribute.standard.Media;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 @Path("/cliente")
 public class ClienteResource {
@@ -14,5 +17,31 @@ public class ClienteResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Cliente> get() {
         return Cliente.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente findById(@QueryParam("id") long id, Request request) {
+        return Cliente.findById(id);
+    }
+
+    @DELETE
+    @Path("deleteById")
+    @Transactional
+    public Response deleteById(@QueryParam("id") long id) {
+        Cliente.deleteById(id);
+        return Response.ok().build();
+    }
+
+    @POST
+    @Transactional
+    @Path("create")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(Cliente cliente) {
+        cliente.id = null; //Garantir sempre que o ID é null para não fazer update em vez de persist
+        Cliente.persist(cliente);
+        return Response.status(Response.Status.CREATED).entity(cliente).build();
     }
 }
